87fa1016cf517a4e16d47ce92875cdae557d54ec
Christian Orozco <christianorozco@Christians-Laptop.local>
2025-06-11 19:09:37 -0700

    Finished updating all files.

---
+++ index.js
@@ -0,0 +1,29 @@
+const convertToCelsius = (farenheight) => {
+    return (farenheight - 32) * 5/9;
+}
+
+const describeTemperature = (farenheight) => {
+    if (convertToCelsius(farenheight) < 0) {
+        str = "very cold";
+    } else if (convertToCelsius(farenheight) < 20) {
+        str = "cold";        
+    } else if (convertToCelsius(farenheight) < 30) {
+        str = "warm";        
+    } else if (convertToCelsius(farenheight) < 40) {
+        str = "hot";
+    } else if (convertToCelsius(farenheight) >= 40) { 
+        str = "very hot";
+    }
+    return str;
+}
+
+
+const userInput = prompt(
+  "Enter a temperature in farenheight. It will then be coverted to celsius.",
+  ""
+);
+
+let farenheight = userInput;
+
+
+window.alert(`The temperature you entered is equivalent to ${convertToCelsius(farenheight)} degrees celsius. It would feel ${describeTemperature(farenheight)}.`);
